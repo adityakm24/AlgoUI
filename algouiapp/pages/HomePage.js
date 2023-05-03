09d8bad349e51a815940ec56979fd7cd1719283d
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-05-04 01:05:06 +0530

    added about me

--- algouiapp/pages/HomePage.js
+++ algouiapp/pages/HomePage.js
@@ -11,12 +11,14 @@ export default function Home() {
           AlgoUI is a web application which is used to test various scheduling
           algorithms in the Minix OS. Users can input parameters such as process
           arrival time and burst time, as well as the quantum size, and see how
-          each algorithm performs under different conditions.
+          each algorithm performs under different conditions.The app is built
+          using Next.js, a popular React-based framework, and is designed to be
+          responsive and easy to use.
         </p>
         <h3>contributors:</h3>
-        <a href="https://github.com/adityakm24"> Aditya Krishnan M</a> / 
+        <a href="https://github.com/adityakm24"> Aditya Krishnan M</a> /
         <a href="https://github.com/team-member-1"> Hamza Shariff</a> /
-        <a href="https://github.com/team-member-1"> Gokul Sangheeth</a> 
+        <a href="https://github.com/team-member-1"> Gokul Sangheeth</a>
       </div>
     </div>
   );
