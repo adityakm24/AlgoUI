b6b86c67933b275105549c128c161a977e529714
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-05-04 07:57:20 +0530

    improving the UI

--- algouiapp/pages/HomePage.js
+++ algouiapp/pages/HomePage.js
@@ -12,13 +12,23 @@ export default function Home() {
           algorithms in the Minix OS. Users can input parameters such as process
           arrival time and burst time, as well as the quantum size, and see how
           each algorithm performs under different conditions.The app is built
-          using <a href="https://nextjs.org/" target="_blank">Next.js</a>, a popular React-based
-          framework, and is designed to be responsive and easy to use.
+          using{" "}
+          <a href="https://nextjs.org/" target="_blank">
+            Next.js
+          </a>
+          , a popular React-based framework, and is designed to be responsive
+          and easy to use.
         </p>
-        <h3>contributors:</h3>
+        <h3>Contributors:</h3>
         <a href="https://github.com/adityakm24"> Aditya Krishnan M</a> /
         <a href="https://github.com/team-member-1"> Hamza Shariff</a> /
-        <a href="https://github.com/team-member-1"> Gokul Sangheeth</a>
+        <a href="https://github.com/team-member-1"> Gokul Sangheeth</a>{" "}
+        <br></br>
+        <br></br>
+        <br></br>
+        <a href="https://github.com/adityakm24/AlgoUI">
+          <p>View Code on GitHub</p>
+        </a>
       </div>
     </div>
   );
